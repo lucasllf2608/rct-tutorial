acdb8b8dd8f96620271df32e5762abc78a337e62
Lucas de Lima <lucas.llf2608@gmail.com>
2020-01-17 13:50:35 -0300

    commit Game

--- Game.jsx
+++ Game.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import Board from './Board'
 import '../style/game.css'
 
- 
+
 export default class Game extends Component {
     render(){
         return(
@@ -18,31 +18,3 @@ export default class Game extends Component {
         );
     }
 }
-
-
-/*
-Oleo do Motor
-Oleo do Filtro
-Filtro de Ar
-Filtro do Combustivel
-FIltro do Ar condicionado
-ALinhamento
-Balanceamento
-Velas de ingnição
-Correa dentada
-Pastilha de freio : 200,00
-Disco de Freio : 350,00
-Fluido do freio
-Correia poly V esticador
-
-
-co
-
-
-
-
-
-
-
-
-*/
